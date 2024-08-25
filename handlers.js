fb8b7dbd1f078a18870c6715cb9da71c40d6d536
mishuagopian <mishuagopian@gmail.com>
2024-08-25 17:43:26 -0300

    feat: added custom names and favicon

--- handlers.js
+++ handlers.js
@@ -1,4 +1,13 @@
 (function() {
+  // Query params names parser
+  const searchParams = new URLSearchParams(window.location.search);
+  const allNames = searchParams.get("i").split(",").map((n) => n[0].toUpperCase() + n.slice(1));
+  const names = allNames.slice(0, -1);
+  const last = allNames[allNames.length - 1];
+  const greetings = document.querySelector(".details .heading");
+  greetings.textContent = `${names.join(", ")} y ${last}`;
+
+  // RSVP handlers
   const rsvpButton = document.getElementById("rsvp");
   rsvpButton.addEventListener("click", (event) => {
     if (rsvpButton.classList.contains("disabled")) return;
