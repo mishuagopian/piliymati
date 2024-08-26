c1bfb045119e72a674cc3d0f345e0c53dfcbdbb0
mishuagopian <mishuagopian@gmail.com>
2024-08-25 21:28:52 -0300

    feat: added presents section

--- handlers.js
+++ handlers.js
@@ -13,6 +13,7 @@
     if (rsvpButton.classList.contains("disabled")) return;
 
     if (rsvpButton.textContent === "CONFIRMAR ASISTENCIA") {
+      rsvpButton.parentElement.classList.add("expanded");
       document.getElementById("rsvp-options").classList.remove("hidden");
       rsvpButton.textContent = "¡CONFIRMAR!";
     } else {
@@ -20,8 +21,38 @@
       rsvpButton.textContent = "...";
       setTimeout(() => {
         rsvpButton.textContent = "¡NOS VEMOS!";
+        rsvpButton.parentElement.classList.remove("expanded");
       document.getElementById("rsvp-options").classList.add("hidden");
     }, 1000);
     }
+  });
+
+  // Present handlers
+  const presentButton = document.getElementById("present");
+  presentButton.addEventListener("click", (event) => {
+    if (presentButton.textContent === "HACER REGALO") {
+      presentButton.parentElement.classList.add("expanded");
+      document.getElementById("present-options").classList.remove("hidden");
+      presentButton.style.display = "none";
+      presentButton.style.height = 0;
+    }
+  });
+
+
+  // Alias handlers
+  const copyToClipboard = (text) => {
+    const tempInput = document.createElement("input");
+    document.body.appendChild(tempInput);
+    tempInput.value = text;
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+    alert("Copiaste los datos de cuenta a tu portapapeles");
+  };
+  document.getElementById("alias-ars").addEventListener("click", () => {
+    copyToClipboard("MISHU.GALICIA");
+  });
+  document.getElementById("alias-usd").addEventListener("click", () => {
+    copyToClipboard("MISHU.GALICIA.D");
   });
 })();
